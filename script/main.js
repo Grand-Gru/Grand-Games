4e81b233ebf96aa4c35560bbdf55ce59e4175347
Gustavo Lima <limagustavorafael@hotmail.com>
2022-05-31 00:33:50 -0300

    reutizado

--- script/main.js
+++ script/main.js
@@ -6,7 +6,8 @@ import { createElements } from "./gameCards.js";
 createElements();
 
 window.onscroll = function (e) {
-    
+    if ((window.innerHeight + window.scrollY + 100) >= document.body.offsetHeight)
+        createElements();
 };
 const button_header = document.getElementById("button_header");
 
@@ -22,9 +23,7 @@ function buttonVerification(id) {
     for(const button of button_header.children){
         if(button.id === id){ 
             createElements(false,false,id)
-            button.className = 'pressed';
-      
-        
+            button.className = 'pressed';  
         }else{
              button.className = '';
         }
