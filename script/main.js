f8f925d293dc4bb895ed246c3072a1bcb99d0325
SirBatatinha <68497472+SirBatatinha@users.noreply.github.com>
2022-06-02 08:15:48 -0300

    indentação

--- script/main.js
+++ script/main.js
@@ -12,58 +12,58 @@ const selectSort = document.getElementById("order");
 window.onscroll = function (e) {
     if ((window.innerHeight + window.scrollY + 100) >= document.body.offsetHeight)
         createElements();
-        
+
 };
 
 selectSort.addEventListener('change', e => changeGames());
 
 
 
-for(const button of button_header.children){ // cria eventos para quando um botão do header é apertado
-    
+for (const button of button_header.children) { // cria eventos para quando um botão do header é apertado
+
     button.addEventListener('click', e => {
-             
-        if(button.id === 'favoritos'){ 
-            document.getElementById("pc").classList.remove("pressed");   
-            document.getElementById("browser").classList.remove("pressed");     
-            document.getElementById("all").classList.remove("pressed");  
-            
-     
-            button.className = 'pressed';  
+
+        if (button.id === 'favoritos') {
+            document.getElementById("pc").classList.remove("pressed");
+            document.getElementById("browser").classList.remove("pressed");
+            document.getElementById("all").classList.remove("pressed");
+
+
+            button.className = 'pressed';
             document.getElementById("game-line").classList.add("game-line-disabled");
-            document.getElementById("banner").innerHTML= `
-            <div class ="favorites-flex">
-                <div class="favorite-line"></div>
-                <img class="favorite-star" src="../img/star-hover.png">
-                <div class="favorite-line"></div>
-            </div>
-        `
-        document.getElementById("games").innerHTML = `   
-        <div class = "mini-banner">
-        <a href="${data.game_url}"> <img src="${data.thumbnail}"  class="card"> </a>
-        <div class="card-content">
-            <div>
-                <span class="release-date">${data.release_date}</span>
-                <div class="text-content">
-                    <h1 class="title">${data.title}</h1>
-                    <span class="description">${data.short_description}</span>
+            document.getElementById("banner").innerHTML = `
+                <div class ="favorites-flex">
+                    <div class="favorite-line"></div>
+                        <img class="favorite-star" src="../img/star-hover.png">
+                    <div class="favorite-line"></div>
                 </div>
-            </div>
-            <button class="star" id="${data.id}" onclick="saveFav(${data.id})"></button> 
-        </div>
-    </div>`;
-        }else{ 
+        `
+            document.getElementById("games").innerHTML = `   
+                <div class = "mini-banner">
+                    <a href="${data.game_url}"> <img src="${data.thumbnail}"  class="card"> </a>
+                    <div class="card-content">
+                        <div>
+                            <span class="release-date">${data.release_date}</span>
+                            <div class="text-content">
+                                <h1 class="title">${data.title}</h1>
+                                <span class="description">${data.short_description}</span>
+                            </div>
+                        </div>
+                        <button class="star" id="${data.id}" onclick="saveFav(${data.id})"></button> 
+                    </div>
+                </div>`;
+        } else {
             document.getElementById("game-line").classList.remove("game-line-disabled");
             buttonVerification(button.id);
         }
     })
 }
 
-for(const sideBarIten of sideBarItens.children) // cria eventos para quando um botão da sideBar é apertado
+for (const sideBarIten of sideBarItens.children) // cria eventos para quando um botão da sideBar é apertado
     sideBarIten.addEventListener('click', e => {
-        for(const tagButton of sideBarItens.children)
+        for (const tagButton of sideBarItens.children)
             tagButton.classList.remove('pressed');
-        sideBarIten.classList.add('pressed'); 
+        sideBarIten.classList.add('pressed');
         changeGames();
     })
 
@@ -79,14 +79,14 @@ const changeGames = () => { // pega os parametros para criar novos elementos
 }
 
 function buttonVerification(id) {
-    for(const button of button_header.children){
-        if(button.id === id){ 
-            button.className = 'pressed';  
+    for (const button of button_header.children) {
+        if (button.id === id) {
+            button.className = 'pressed';
         }
-        else{
-             button.className = '';
+        else {
+            button.className = '';
         }
-    }    
+    }
     changeGames();
 
 }
