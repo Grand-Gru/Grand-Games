ff93e1b24fac64861b67a62ef7a56a9aeccd6d44
Gustavo Lima <limagustavorafael@hotmail.com>
2022-06-06 21:18:45 -0300

    estrela-banner

--- script/gameCards.js
+++ script/gameCards.js
@@ -3,6 +3,41 @@
 import { GAMES_DATA, filterGameInfoByTag } from "./gameData.js";
 
 
+const createBanner = data => {// altera o banner
+    if(data){ 
+    const releaseDate = new Date(data.release_date);
+    document.getElementById("banner").innerHTML= `
+        <video class="featuredvideo" loop autoplay muted="" poster="${data.thumbnail}">
+            <source src="https://www.freetogame.com/g/${data.id}/videoplayback.webm" type="video/webm">
+        </video>
+        <div class="game-info">
+        <div class="star-flex">
+                <span class="game-title">${data.title}</span>
+                <button class="star" id="${data.id}" onclick="saveFav(${data.id})"></button> 
+        </div> 
+                <p class="game-desciption">${data.short_description}</p>
+          
+      
+            <div class="footer">
+            <a href="${data.game_url}" target="_blank">Acessar Jogo</a>
+            <span class="developer">${data.developer} ${releaseDate.getFullYear()}</span>
+        </div>
+    </div>
+    `
+    const favorites = JSON.parse(localStorage.favorites);
+    if(favorites.includes(data.id))
+        document.getElementById(data.id).classList.add("shining-star");
+    
+    }else{
+        document.getElementById("game-line").classList.add("game-line-disabled");
+        document.getElementById("banner").innerHTML= `
+        <div class ="error-message">
+        <h2>There are no games in those sections.</h2>
+     </div>
+    `
+    }
+}
+
 const createCard = data => { // cria o card e coloca a thumbmail
     document.getElementById("games").innerHTML += `
     <div class = "mini-banner">
@@ -20,33 +55,6 @@ const createCard = data => { // cria o card e coloca a thumbmail
     </div>`;
 }
 
-
-const createBanner = data => {// altera o banner
-    if(data){ 
-    const releaseDate = new Date(data.release_date);
-    document.getElementById("banner").innerHTML= `
-        <video class="featuredvideo" loop autoplay muted="" poster="${data.thumbnail}">
-            <source src="https://www.freetogame.com/g/${data.id}/videoplayback.webm" type="video/webm">
-        </video>
-        <div class="game-info">
-        <span class="game-title">${data.title}</span>
-        <p class="game-desciption">${data.short_description}</p>
-        <div class="footer">
-            <a href="${data.game_url}" target="_blank">Acessar Jogo</a>
-            <span class="developer">${data.developer} ${releaseDate.getFullYear()}</span>
-        </div>
-    </div>
-    `
-
-    }else{
-        document.getElementById("game-line").classList.add("game-line-disabled");
-        document.getElementById("banner").innerHTML= `
-        <div class ="error-message">
-        <h2>There are no games in those sections.</h2>
-     </div>
-    `
-    }
-}
 
 
 const filterGamesInfo = async (tag, sortMethod, platform, fav = false) => { // filtra as informações do jogo
